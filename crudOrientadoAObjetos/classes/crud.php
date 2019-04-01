39ac6dc29e8a027df93ce5200cf1a2f1aa1edff0
romarioL <45340806+romarioL@users.noreply.github.com>
2019-04-01 19:11:01 -0300

    Add files via upload

---
+++ crudOrientadoAObjetos/classes/crud.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once 'DB.php';
+
+abstract class crud  extends DB {
+
+	protected $table;
+
+	abstract public function insert();
+	abstract public function update($id);
+
+
+	public function find($id) {
+      
+
+      $sql = "SELECT * FROM $this->table  WHERE id = :id";
+      $statement= DB::prepare($sql);
+      $statement->bindParam(':id', $id, PDO::PARAM_INT);
+      $statement->execute();
+
+      return $statement->fetch();
+	}
+
+	public function findAll() {
+      
+
+      $sql = "SELECT * FROM $this->table";
+      $statement= DB::prepare($sql);
+      $statement->execute();
+
+      return $statement->fetchAll();
+	}
+
+
+	public function delete($id) {
+
+	  $sql = "DELETE  FROM $this->table  WHERE id = :id";
+      $statement= DB::prepare($sql);
+      $statement->bindParam(':id', $id, PDO::PARAM_INT);
+      $statement->execute();
+
+	}
+
+}
+
+ ?>
---
+++ crudOrientadoAObjetos/classes/usuarios.php
@@ -0,0 +1,47 @@
+<?php
+
+require_once 'crud.php';
+
+class usuarios extends crud {
+	protected $table = 'usuarios';
+	private $nome;
+	private $email;
+
+	public function setNome($nome) {
+       $this->nome = $nome;
+	}
+
+	public function getNome() {
+		return $this->nome;
+	}
+
+	public function setEmail($email) {
+       $this->email = $email;
+	}
+
+	public function getEmail() {
+		return $this->email;
+	}
+
+	public function insert() {
+       $sql = "INSERT INTO $this->table(nome, email) VALUES (:nome, :email)";
+       $statement = BD::prepare($sql);
+       $statement->bindParam(':nome', $this->nome);
+       $statement->bindParam(':email', $this->email);
+
+       return $statement->execute();
+	}
+
+	public function update($id) {
+       $sql = "INSERT INTO $this->table SET nome = :nome, email = :email WHERE id =:id";
+       $statement = DB::prepare($sql);
+       $statement->bindParam(':nome', $this->nome);
+       $statement->bindParam(':email', $this->email);
+       $statement->bindParam(':id', $id);
+
+
+       return $statement->execute();
+	}
+}
+
+ ?>
